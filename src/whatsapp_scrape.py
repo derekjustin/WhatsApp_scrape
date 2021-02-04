c5a6f27fbe721297b6aa51666e69ae9d591c1cf2
jstrelka <justin.strelka@outlook.com>
2021-02-03 19:55:04 -0700

    reorganized repo and created main method

--- src/whatsapp_scrape.py
+++ src/whatsapp_scrape.py
@@ -4,12 +4,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
-options = webdriver.ChromeOptions()
-options.add_argument("user-data-dir=/home/jstrelka/.config/google-chrome")
-driver = webdriver.Chrome(executable_path='src/browser_driver/chromedriver', chrome_options=options)
-driver.get("https://web.whatsapp.com/")
+def main():
 
+    profile_path = "user-data-dir=/home/jstrelka/.config/google-chrome"
+    driver_path = 'src/browser_drivers/chromedriver'
+    url = "https://web.whatsapp.com/"
 
-time.sleep(10)
+    options = webdriver.ChromeOptions()
+    options.add_argument(profile_path)
+    driver = webdriver.Chrome(executable_path=driver_path, chrome_options=options)
+    driver.get(url)
+    time.sleep(20)
+    driver.quit()
 
-driver.quit()
+main()
