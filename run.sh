78fb3f101fe39493bcf5b1efa42ab80ac762db43
jstrelka <justin.strelka@outlook.com>
2021-02-10 08:49:55 -0700

    created print_tools.py and added to run.sh

--- run.sh
+++ run.sh
@@ -13,17 +13,4 @@ echo "####################################\n"
 # Run tests!
 py.test
 
-
-echo "\n\n######################################################################"
-echo "    TO RUN A FEATURE OF THE WEB SCRAPER PLEASE ENSURE"
-echo "        1) You are currently logged in to your Google Account through Chrome"
-echo "        2) You are using Chrome Version XXXXX"
-echo "        3) You are logged into WhatsApp Web (https://web.whatsapp.com)"
-echo "        4) The Google Chrome Browser is currently closed\n"
-echo "    Once the steps above are verified execute one of the commands" 
-echo "        below to start scraping!" 
-echo "######################################################################\n"
-
-echo "   group_tools"
-echo "      description: Open GroupTools menu\n"
-
+python scrape/print_tools.py
---
+++ scrape/print_tools.py
@@ -0,0 +1,13 @@
+def print_endpoints():
+    print("\n\n" + 80 * '#' + "\n" +
+    "TO RUN A FEATURE OF THE WEB SCRAPER PLEASE ENSURE\n" +
+    "\t1) You are currently logged in to your Google Account through Chrome\n" +
+    "\t2) You are using Chrome Version XXXXX\n" +
+    "\t3) You are logged into WhatsApp Web (https://web.whatsapp.com)\n" +
+    "\t4) The Google Chrome Browser is currently closed\n" +
+    "Once the steps above are verified execute one of the commands\n" +
+    "below to start scraping!\n" +
+    80 * '#' + "\n\n" +
+    "\tgroup_tools\n\t\tdescription: Open GroupTools menu\n\n")
+
+print_endpoints()
