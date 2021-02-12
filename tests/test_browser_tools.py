8af1c8ee839cd313983bbadd4b23416eb023d0d9
Justin Strelka <justin.strelka@outlook.com>
2021-02-12 11:11:35 -0700

    Dhdev (#12)
    * Found library to take care of driver for MacOS
    
    * Made the application MacOS capable
    
    * deleted the drivers and replaced with a imported library
    
    * cleaned up code
    
    * fixed and wrote tests, still one test broken
    
    * fixed test in test_browser_tools.py
    
    * removed todo commenting in test_browser_tools.py
    
    Co-authored-by: Derek Holsapple <derekholsapple@Dereks-MacBook.local>
    Co-authored-by: holsappled <derekhols31@gmail.com>

--- tests/test_browser_tools.py
+++ tests/test_browser_tools.py
@@ -29,15 +29,12 @@ def test_check_browser_status():
     assert alive_status == "Alive"
     assert browser_tools.check_browser_status() == "Dead"
 
-#TODO: Fix this test. After using the "webdriver_manager" library we are experiencing failures
 def test_browser_find_element_by_xpath_with_wait():
     browser_tools = BrowserTools()
     browser_tools.open_browser()
     browser_tools.go_to_url("https://google.com")
-    #I dont think the test is finding the xpath
     found_element = browser_tools.browser_find_element_by_xpath_with_wait("//input[@value='Google Search']")
     browser_tools.close_browser()
-    #Or its whatever this magic is doing "webdriver.remote.webelement.WebElement"
     assert type(found_element) == webdriver.remote.webelement.WebElement
 
 def test_browser_find_element_by_link_text_with_wait():
