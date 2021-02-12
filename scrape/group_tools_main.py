6eebe362a92c719abeb367e9ede6d0aa7a80c935
jstrelka <justin.strelka@outlook.com>
2021-02-12 09:54:19 -0700

    clarified group_tools interface menu

--- scrape/group_tools_main.py
+++ scrape/group_tools_main.py
@@ -5,12 +5,12 @@ def main():
     tool = GroupTools()
     while True:
         print("\n" + 10 * "#" + " GROUP TOOLS MENU " + 10 * "#" + "\nPlease choose one of the following options\n")
-        user_input = input("\njoin_group: joins a single WhatsApp group\n\njoin_multiple_groups: joins all groups listed in a file\n\nquit: end program\n\nUSER INPUT: ")
+        user_input = input("\njoin_group: joins a single WhatsApp group\n\njoin_multiple_groups: joins all groups listed in a file\n\nback: return to main menu\n\nUSER INPUT: ")
         if user_input == ("join_group"):
             tool.join_group()
         elif user_input == ("join_multiple_groups"):
             tool.join_multiple_groups()
-        elif user_input == ("quit"):
+        elif user_input == ("back"):
             break
         else:
             print("\n\n******** PLEASE ENTER A VALID OPTION OR 'quit' TO END PROGRAM.\n")
