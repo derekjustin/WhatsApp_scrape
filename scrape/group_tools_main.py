d63cf50cd69d6acbcbab34acc6d9d6a851a6588d
jstrelka <justin.strelka@outlook.com>
2021-02-22 09:54:40 -0700

    added functionality to retrieve raw html for single group

--- scrape/group_tools_main.py
+++ scrape/group_tools_main.py
@@ -7,7 +7,8 @@ def main():
         print("\n" + 10 * "#" + " GROUP TOOLS MENU " + 10 * "#" + "\nPlease choose one of the following options\n")
         user_input = input("\njoin_group: joins a single WhatsApp group\n\n" +
                            "join_multiple_groups: joins all groups listed in a file\n\n" +
-                           "groups_data: retrieves and saves raw html containing group messages\n\n" +
+                           "single_group_data: retrieves and saves raw html containing single group messages\n\n" +                           
+                           "all_groups_data: retrieves and saves raw html containing group messages for all groups\n\n" +
                            "print_groups: prints group list to the console\n\n" +
                            "back: return to main menu\n\nUSER INPUT: ")
         print("")
@@ -15,7 +16,9 @@ def main():
             tool.join_group()
         elif user_input == ("join_multiple_groups"):
             tool.join_multiple_groups()
-        elif user_input == ("groups_data"):
+        elif user_input == ("single_group_data"):
+            tool.save_single_groups_data()
+        elif user_input == ("all_groups_data"):
             tool.save_all_groups_data()
         elif user_input == ("print_groups"):
             tool.print_groups()
