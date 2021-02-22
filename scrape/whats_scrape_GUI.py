8c00a8741ce2a2909d4908cb7a41523413e8cdd3
jstrelka <justin.strelka@outlook.com>
2021-02-22 15:17:19 -0700

    continued to build on gui

--- scrape/whats_scrape_GUI.py
+++ scrape/whats_scrape_GUI.py
@@ -4,15 +4,21 @@ layout = [
             [sg.Column([[sg.Text('Welcome to the WhatsApp Web Scraping Tool', font=['Comic',30,'bold'], text_color='#ffffff', background_color='#2e9688')]],
                 background_color='#2e9688'
                 )],
-            [sg.Column([[sg.Text('What Would You Like To Do?', font=['Comic',20,'bold'], text_color='#000000', background_color='#d8dbd4')]],
+            [sg.Column([[sg.Text('Please Choose One Of The Following Options!', font=['Comic',20,'bold'], text_color='#000000', background_color='#d8dbd4')]],
+                background_color='#d8dbd4',
+                justification='center',
+                pad=(50,50)
+                )],
+            [sg.Column([[sg.Button('Join Group'),
+                         sg.Button('Join Multiple Groups'), 
+                         sg.Button('Print Groups')]],
                 background_color='#d8dbd4',
                 justification='center'
-                )],
-            [sg.Button('Ok'), sg.Button('Cancel')]
+                )]
             ]
 
 # Create the Window
-window = sg.Window('Window Title', layout=layout, background_color='#d8dbd4', margins=[0,0], element_padding=0)
+window = sg.Window('Window Title', layout=layout, background_color='#d8dbd4', button_color=['#000000','#2e9688'], margins=[0,0])
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
