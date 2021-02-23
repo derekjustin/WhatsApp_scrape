a4b798c11a2ba10a3ad1712784c242b15826d2ef
holsappled <derekhols31@gmail.com>
2021-02-22 21:08:08 -0700

    added functionality to processing individual html files

--- scrape/scrape_html.py
+++ scrape/scrape_html.py
@@ -1,18 +1,17 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-A test of beautiful soup 
-"""
-
 from bs4 import BeautifulSoup
 from datetime import datetime
 import re
 import pandas as pd
+import os
+
+from scrape.system_tools import SystemTools
 
 class WhatsAppHtmlParser:
 
     def __init__(self, html_file_name):
-        with open("/home/garage/Desktop/WhatsApp_scrape/scrape/group_files/raw_html_files/" + html_file_name , 'r') as f:
+        self.sys_tools = SystemTools()
+       # print (self.sys_tools.get_html_dir_path + '/' + html_file_name)
+        with open(self.sys_tools.get_html_dir_path() + '/' + html_file_name , 'r') as f:
             contents = f.read()
         self.soup = BeautifulSoup(contents, 'html.parser')
         
@@ -32,6 +31,10 @@ class WhatsAppHtmlParser:
         df = df.set_index('Datetime')    
         return df  
     
+
+
+
+
     def save_pd_frame_to_pickle(self, df, file_name, dest_folder ):
         df.to_pickle( dest_folder + "/" + file_name + ".pkl")
     
@@ -44,7 +47,6 @@ class WhatsAppHtmlParser:
     def read_csv_to_frame(self, file_path):
         df = pd.read_csv(file_path) 
         return df.set_index('Datetime')
-        
     
     def find_full_message_with_string(self, instance_of ):         
         return self.soup.find(string=re.compile(instance_of)) 
@@ -66,13 +68,44 @@ class WhatsAppHtmlParser:
                 if searchFor in v:
                     return k
         return None        
-            
-tester = WhatsAppHtmlParser("20210222+92 306 5249518")        
-theThing = tester.get_messages_pd_frame()
-message = tester.find_full_message_with_string( "How" )     
-tester.print_html()
-tester.save_pd_frame_to_pickle( theThing, "DerekRulz", "/home/garage/Desktop" )
-someFrame = tester.read_pickle_to_frame("/home/garage/Desktop/DerekRulz.pkl")
-tester.save_pd_frame_to_csv( theThing, "DerekRulz", "/home/garage/Desktop" )
-someOtherFrame = tester.read_csv_to_frame("/home/garage/Desktop/DerekRulz.csv")
 
+
+class MultiProcessHtml:
+
+    def __init__(self):
+        self.sys_tools = SystemTools()
+        self.raw_html_list = self.sys_tools.get_raw_html_list()
+        print(self.raw_html_list)
+        for html_file in self.raw_html_list:
+            print(type(html_file))
+        self.html_list = self.sys_tools.get_raw_html_list()
+        self.html_pickle_dir = self.sys_tools.get_processed_html_pickles_dir()
+
+
+    def process_all_raw_html_to_pickles(self):
+   #     sys_tools = SystemTools()
+   #     raw_html_list = sys_tools.get_raw_html_list()
+        self.sys_tools.clean_processed_pickle_dir()
+
+        for html_file in self.raw_html_list:
+            html_parser = WhatsAppHtmlParser(html_file)
+            df = html_parser.get_messages_pd_frame()
+            html_parser.save_pd_frame_to_pickle(df, html_file, self.html_pickle_dir )
+    
+#    def clean_processed_pickle_dir(self):        
+#        if not os.path.isdir(self.html_pickle_dir):
+#            os.mkdir(self.sys_tools.get_processed_html_pickles_dir)
+#        else:
+#            sys_tools.delete_all_files_in_dir(self.html_pickle_dir)
+
+#tester = WhatsAppHtmlParser("20210222+92 306 5249518")        
+#theThing = tester.get_messages_pd_frame()
+#message = tester.find_full_message_with_string( "How" )     
+#tester.print_html()
+#tester.save_pd_frame_to_pickle( theThing, "DerekRulz", "/home/garage/Desktop" )
+#someFrame = tester.read_pickle_to_frame("/home/garage/Desktop/DerekRulz.pkl")
+#tester.save_pd_frame_to_csv( theThing, "DerekRulz", "/home/garage/Desktop" )
+#someOtherFrame = tester.read_csv_to_frame("/home/garage/Desktop/DerekRulz.csv")
+stuff = MultiProcessHtml()
+#test = WhatsAppHtmlParser('2021022219:26:32_Testing')
+stuff.process_all_raw_html_to_pickles()
--- scrape/system_tools.py
+++ scrape/system_tools.py
@@ -1,6 +1,6 @@
 from appdirs import *
 import platform
-
+import os, re, os.path
 
 class SystemTools:
     #############
@@ -31,3 +31,25 @@ class SystemTools:
 
     def get_sys_platform(self):
         return self.platform_os
+
+    def get_html_dir_path(self):
+        return self.get_cwd_path() + '/group_files/raw_html_files'
+
+    def get_raw_html_list(self):
+        html_dir = self.get_html_dir_path()
+        return os.listdir(html_dir)
+
+    def get_processed_html_pickles_dir(self):
+        return self.get_cwd_path() + '/group_files/processed_html_pickles'
+
+    def delete_all_files_in_dir(self, myfolder):
+        mypath = myfolder
+        for root, dirs, files in os.walk(mypath):
+            for file in files:
+                os.remove(os.path.join(root, file))
+
+    def clean_processed_pickle_dir(self):        
+        if not os.path.isdir(self.get_processed_html_pickles_dir()):
+            os.mkdir(self.get_processed_html_pickles_dir())
+        else:
+            self.delete_all_files_in_dir(self.get_processed_html_pickles_dir())
