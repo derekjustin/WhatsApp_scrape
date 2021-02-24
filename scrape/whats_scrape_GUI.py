827066d83b5fff806a42a550c0c317b4045919c5
jstrelka <justin.strelka@outlook.com>
2021-02-24 07:25:51 -0700

    made style changes to GUI files

--- scrape/whats_scrape_GUI.py
+++ scrape/whats_scrape_GUI.py
@@ -10,23 +10,13 @@ class WhatsScrapeGUI:
 
     def make_home_menu(self):
         layout = [
-            [sg.Column([[sg.Text('Welcome to the WhatsApp Web Scraping Tool', font=['Comic',30,'bold'], text_color='#ffffff', background_color='#2e9688')]],
-                    background_color='#2e9688'
-                )],
-            [sg.Column([[sg.Text('Please Choose One Of The Following Options!', font=['Comic',20,'bold'], text_color='#000000', background_color='#d8dbd4')]],
-                background_color='#d8dbd4',
-                justification='center',
-                pad=(50,50)
-                )],
+            [sg.Column([[sg.Text('Welcome to the WhatsApp Web Scraping Tool', font=['Comic', 30, 'bold'], text_color='#ffffff', background_color='#2e9688')]], background_color='#2e9688')],
+            [sg.Column([[sg.Text('Please Choose One Of The Following Options!', font=['Comic', 20, 'bold'], text_color='#000000', background_color='#d8dbd4')]], background_color='#d8dbd4', justification='center', pad=(50, 50))],
             [sg.Column([[sg.Button('Join Group'),
-                    sg.Button('Join Multiple Groups'), 
-                    sg.Button('Print Groups'),
-                    sg.Button('Quit')]],
-                background_color='#d8dbd4',
-                justification='center'
-                )]
-            ]
-        return sg.Window('Window Title', layout=layout, background_color='#d8dbd4', button_color=['#000000','#2e9688'], margins=[0,0])
+                         sg.Button('Join Multiple Groups'),
+                         sg.Button('Print Groups'),
+                         sg.Button('Quit')]], background_color='#d8dbd4', justification='center')]]
+        return sg.Window('Window Title', layout=layout, background_color='#d8dbd4', button_color=['#000000', '#2e9688'], margins=[0, 0])
 
     def make_print_groups_screen(self):
         group_list = self.group_tools.get_group_list()
@@ -34,14 +24,7 @@ class WhatsScrapeGUI:
         for group in group_list:
             group_text += (group + "\n")
         layout = [
-            [sg.Column([[sg.Text('Your Joined Group List', font=['Comic',20,'bold'], text_color='#ffffff', background_color='#2e9688')]],
-                    background_color='#2e9688'
-                )],
-            [sg.Column([[sg.Multiline(default_text=group_text,auto_size_text=True, size=(20,30))]]
-                )],
-            [sg.Column([[sg.Button('Return')]], 
-                background_color='#d8dbd4',
-                justification='center'
-                )]
-            ]
-        return sg.Window('Printed Groups', layout=layout, background_color='#d8dbd4', button_color=['#000000','#2e9688'], margins=[0,0])
+            [sg.Column([[sg.Text('Your Joined Group List', font=['Comic', 20, 'bold'], text_color='#ffffff', background_color='#2e9688')]], background_color='#2e9688')],
+            [sg.Column([[sg.Multiline(default_text=group_text, auto_size_text=True, size=(20, 30))]])],
+            [sg.Column([[sg.Button('Return')]], background_color='#d8dbd4', justification='center')]]
+        return sg.Window('Printed Groups', layout=layout, background_color='#d8dbd4', button_color=['#000000', '#2e9688'], margins=[0, 0])
--- scrape/whats_scrape_GUI_main.py
+++ scrape/whats_scrape_GUI_main.py
@@ -1,9 +1,10 @@
 from scrape.whats_scrape_GUI import WhatsScrapeGUI
 import time
 
+
 def main():
     gui = WhatsScrapeGUI()
-    
+
     while True:
         window = gui.make_home_menu()
         event, values = window.read()
@@ -12,7 +13,7 @@ def main():
             window.close()
             exit(0)
         elif event == "Print Groups":
-            window = gui.make_print_groups_screen() 
+            window = gui.make_print_groups_screen()
             event, values = window.read()
             if event == 'Return':
                 window.close()
